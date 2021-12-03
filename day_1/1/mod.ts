42e001d77aff1fd1ace05c31a12c02313aef4597
Rodney van den Velden <rodneymichael2002@gmail.com>
2021-12-03 16:53:57 +0100

    Day One

---
+++ day_1/1/mod.ts
@@ -0,0 +1,19 @@
+let cur = 0;
+let prev = 0;
+let count = 0;
+
+function countMeasures(measurements: string[]) {
+    for(const measure of measurements) {
+        cur = parseInt(measure);
+        if(cur > prev && prev !== 0) {
+            count++
+        }
+        prev = cur;
+    }
+    console.log(count);
+}
+
+const text = Deno.readTextFileSync('input.txt');
+const textByLine = text.split('\n');
+
+countMeasures(textByLine);
---
+++ day_1/2/mod.ts
@@ -0,0 +1,33 @@
+let cur = 0;
+let curSum = 0;
+const prevArray: number[] = [];
+let prevSum = 0;
+let count = 0;
+
+function countMeasures(measurements: string[]) {
+    for(const measure of measurements) {
+        cur = parseInt(measure);
+
+        prevArray.push(cur);
+        
+        if(prevArray.length > 3) {
+            prevArray.shift();
+            curSum = 0;
+            for(const num of prevArray) {
+                curSum += num;
+            }
+        }
+
+        if(curSum > prevSum) {
+            count++;
+        }
+
+        prevSum = curSum;
+    }
+    console.log(count);
+}
+
+const text = Deno.readTextFileSync('input.txt');
+const textByLine = text.split('\n');
+
+countMeasures(textByLine);
